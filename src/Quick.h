d4994f425fe01069a460d2c94ce19a4015fe96f3
Michalina Bączek <123661@uksw.edu.pl>
2025-05-28 15:38:50 +0200

    QuickSort_123661_2

--- src/Quick.h
+++ src/Quick.h
@@ -1,11 +1,14 @@
 #ifndef _QUICK_H
 #define _QUICK_H
+
 #include "SortTech.h"
 #include "SortError.h"
 #include "SortData.h"
 
 template<class T, template<typename...> class Container>
 class Quick : public SortTech<T, Container> {
+public:
+    inline SortError sort(SortData<T>& data) override;
 
     template <typename T>
     void quickSort(SortData<T>& data) {
@@ -37,6 +40,10 @@ class Quick : public SortTech<T, Container> {
     }
 };
 template<class T, template<typename...> class Container>
+inline SortError Quick<T, Container>::sort(SortData<T>& data) {
 
+    quickSort(data);
+    return SE_SUCCESS;
 }
 
+#endif
