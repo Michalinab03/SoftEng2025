ee9a8cfe5f1b254f325f583c1420c45ddb59cba6
Michalina Bączek <123661@uksw.edu.pl>
2025-05-28 17:34:51 +0200

    QuickSort_123661_test

--- src/tests/test_quick.cpp
+++ src/tests/test_quick.cpp
@@ -5,9 +5,16 @@
 
 TEST(test_quick, AddFunction)
 {
-    SortError error = SE_SUCCESS;
+    Sorter* sorter = new Sorter();
 
-    // add your test here
+    SortData<int, std::vector> data;
+
+    std::vector<int> L1{ 1, 233, 3, 87, 44, 255, 111, 11, 32 };
+
+    const decltype(L1)::value_type* d = L1.data();
+    data.add(d, L1.size());
+
+    auto error = sorter->sort<int, std::vector, STT_QUICK>(data);
 
     ASSERT_EQ(error, SE_SUCCESS);
 }
