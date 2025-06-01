2160a09f7f34b7df9d74bfef674d8d87f7b2247e
Michalinab03 <michalina.baczek3@wp.pl>
2025-06-01 12:30:30 +0200

    Update test_quick.cpp

--- src/tests/test_quick.cpp
+++ src/tests/test_quick.cpp
@@ -16,6 +16,12 @@ TEST(test_quick, AddFunction)
 
     auto error = sorter->sort<int, std::vector, STT_QUICK>(data);
 
+    const std::vector<int> sorted_vec=data.get();
+
+    for (size_t i=1; i<sorted_vec.size(); ++i){
+        assert(sorted_vec[i-1]<=sorted_vec[i]);
+    }
+    
     ASSERT_EQ(error, SE_SUCCESS);
 }
 
